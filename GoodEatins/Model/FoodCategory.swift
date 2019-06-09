37db9faee6be9fb5ca4e5113b90a7a932c4db484
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-09 12:27:48 +0900

    ベース作成

---
+++ GoodEatins/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatins
+//
+//  Created by 大石耕司 on 2019/06/09.
+//  Copyright © 2019 大石耕司. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title : String
+    let imageName : String
+}
---
+++ GoodEatins/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatins
+//
+//  Created by 大石耕司 on 2019/06/09.
+//  Copyright © 2019 大石耕司. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var instructions: String
+    var imageName: String
+}
