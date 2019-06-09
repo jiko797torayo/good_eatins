62b6d854bb4ecfc91eab074a5c8b86262ca1c243
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-06-09 16:45:12 +0900

    Recipe画面ベース作成

---
+++ GoodEatins/View/RecipeCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeCell.swift
+//  GoodEatins
+//
+//  Created by 大石耕司 on 2019/06/09.
+//  Copyright © 2019 大石耕司. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recipeImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        recipeImg.layer.cornerRadius = 10
+    }
+    
+    func configureCell(recipe: Recipe) {
+       recipeImg.image = UIImage(named: recipe.imageName)
+    }
+    
+}
